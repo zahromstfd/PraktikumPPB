391d35b96685f78d1fd2b6923aeb50aefc21ade8
zahromstfd <zahromustafidah27@gmail.com>
2023-04-19 06:40:41 +0700

    Praktikum PPB Modul 5 RecyclerView

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.recyclerview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Telephony
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.R.id.rv_sekolah
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var rvSekolah: RecyclerView
+    private var list: ArrayList<Sekolah> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvSekolah = findViewById(rv_sekolah)
+        rvSekolah.setHasFixedSize(true)
+
+        list.addAll(DataSekolah.listData)
+     //   showRecyclerViewListMode()
+    //    showRecyclerViewGridMode()
+            showRecyclerViewCardMode()
+    }
+    private fun showRecyclerViewListMode(){
+        rvSekolah.layoutManager = LinearLayoutManager(this)
+        val listSekolahAdapter = ListSekolahAdapter(list)
+        rvSekolah.adapter = listSekolahAdapter
+    }
+    private fun showRecyclerViewGridMode(){
+        rvSekolah.layoutManager = LinearLayoutManager(this)
+        val gridSekolahAdapter = GridSekolahAdapter(list)
+        rvSekolah.adapter = gridSekolahAdapter
+    }
+    private fun showRecyclerViewCardMode(){
+        rvSekolah.layoutManager = LinearLayoutManager(this)
+        val cardSekolahAdapter = CardSekolahAdapter(list)
+        rvSekolah.adapter = cardSekolahAdapter
+    }
+}
